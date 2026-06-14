e2e2b115efdc98a0427ef2d62f29977fea3d3f78
sisizov-dev <sisizov@gmail.com>
2026-06-14 22:43:43 +0100

    add_test_click_logo

--- tests/test_navigate_from_account_to_constructor.py
+++ tests/test_navigate_from_account_to_constructor.py
@@ -6,3 +6,9 @@ def test_click_constructor(authorized_main_page): #проверяем перех
 
     time.sleep(2)
     assert (authorized_main_page.find_elements(By.XPATH, "//h1[contains(text(),'Соберите бургер')]"))
+
+def test_click_logo(authorized_main_page): #проверяем переход по кнопке логотипа Stellar Burgers
+    authorized_main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click() #нажимаем кнопку личный кабинет
+    authorized_main_page.find_element(By.CLASS_NAME, "AppHeader_header__logo__2D0X2").click() #логотип Stellar Burgers
+    time.sleep(2)
+    assert (authorized_main_page.find_elements(By.XPATH, "//h1[contains(text(),'Соберите бургер')]"))
