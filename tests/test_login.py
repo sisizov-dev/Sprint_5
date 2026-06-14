09a91e682d88defb107707536ab319a18b29b7d8
sisizov-dev <sisizov@gmail.com>
2026-06-14 21:20:40 +0100

    add_test_login_account_icon

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,3 +13,15 @@ def test_login_main_button(main_page, existing_user): #проверяем вхо
     assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
     #assert main_page.find_element(By.XPATH, "//button[(text(),'Оформить заказ')]").is_displayed()
     #assert main_page.current_url == "https://stellarburgers.education-services.ru/"
+
+def test_login_account_icon(main_page, existing_user): #проверяем вход через кнопку «Личный кабинет»
+    main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click() # нажимаем кнопку личный кабинет на главной странице
+
+    #заполняем поля
+    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
+    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
+
+    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
+
+    time.sleep(4) 
+    assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
