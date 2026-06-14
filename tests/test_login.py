6abb2bcc485042e946c43b396c379ac6a7a4df4c
sisizov-dev <sisizov@gmail.com>
2026-06-14 21:41:35 +0100

    add_test_login_from_password_recovery

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,8 +11,8 @@ def test_login_main_button(main_page, existing_user): #проверяем вхо
 
     time.sleep(4) 
     assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
-    #assert main_page.find_element(By.XPATH, "//button[(text(),'Оформить заказ')]").is_displayed()
-    #assert main_page.current_url == "https://stellarburgers.education-services.ru/"
+    
+    
 
 def test_login_account_icon(main_page, existing_user): #проверяем вход через кнопку «Личный кабинет»
     main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click() # нажимаем кнопку личный кабинет на главной странице
@@ -29,7 +29,7 @@ def test_login_account_icon(main_page, existing_user): #проверяем вх�
 def test_login_from_registration_page(main_page, existing_user): #проверяем в ход в личный кабинет через кнопку войти на стрнице регистрации
     main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
     main_page.find_element(By.CLASS_NAME, "Auth_link__1fOlj").click() # нажимаем кнопку зарегистрироваться
-    main_page.find_element(By.XPATH, "//a[text()='Войти']").click()
+    main_page.find_element(By.XPATH, "//a[text()='Войти']").click() #нажимаем кнопку
 
     #заполняем поля
     main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
@@ -39,3 +39,17 @@ def test_login_from_registration_page(main_page, existing_user): #проверя
 
     time.sleep(4) 
     assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
+
+def test_login_from_password_recovery(main_page, existing_user): # проверка входа через кнопку в форме восстановления пароля
+    main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
+    main_page.find_element(By.XPATH, "//a[text()='Восстановить пароль']").click() #нажимаем кнопку восстановить пароль
+    main_page.find_element(By.XPATH, "//a[text()='Войти']").click() #нажимаем кнопку Войти
+
+    #заполняем поля
+    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
+    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
+
+    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
+
+    time.sleep(4) 
+    assert main_page.current_url == "https://stellarburgers.education-services.ru/"
