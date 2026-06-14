558f6a3de8ffd1b907ef88bee406f30f28230df9
sisizov-dev <sisizov@gmail.com>
2026-06-14 21:29:45 +0100

    add_test_login_from_registration_page

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,3 +25,17 @@ def test_login_account_icon(main_page, existing_user): #проверяем вх�
 
     time.sleep(4) 
     assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
+
+def test_login_from_registration_page(main_page, existing_user): #проверяем в ход в личный кабинет через кнопку войти на стрнице регистрации
+    main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
+    main_page.find_element(By.CLASS_NAME, "Auth_link__1fOlj").click() # нажимаем кнопку зарегистрироваться
+    main_page.find_element(By.XPATH, "//a[text()='Войти']").click()
+
+    #заполняем поля
+    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
+    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
+
+    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
+
+    time.sleep(4) 
+    assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
