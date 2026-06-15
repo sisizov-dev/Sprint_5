94ba11c584c09edbaefae6ecc81c70d7a5efc813
sisizov-dev <sisizov@gmail.com>
2026-06-15 21:19:34 +0100

    upgrade_test_sauces_section

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
 import time
-def test_sauces_section(main_page, authorized_main_page): # проверяем переход к разделу соусы
-    main_page.find_element(By.XPATH, "//span[text()='Соусы']").click()
+def test_sauces_section(authorized_main_page): # проверяем переход к разделу соусы
+    authorized_main_page.find_element(By.XPATH, "//span[text()='Соусы']").click()
 
     time.sleep(2)
-    assert main_page.find_element(By.XPATH, "//*[@alt='Соус фирменный Space Sauce']").is_displayed()
+    assert authorized_main_page.find_element(By.XPATH, "//*[@alt='Соус фирменный Space Sauce']").is_displayed()
+
