b4079984902aa7e1127cb2c85dfccf1f5ffc9088
sisizov-dev <sisizov@gmail.com>
2026-06-19 23:20:59 +0100

    add_class_LoginLocators.py

--- tests/locators.py
+++ tests/locators.py
@@ -20,31 +20,21 @@ class LoginViaAccountLocators:
     SUBMIT_LOGIN = (By.XPATH, "//button[text()='Войти']")               # кнопка «Войти»
     TITLE_ORDER = (By.XPATH, "//button[contains(text(),'Оформить заказ')]")  # проверка успешного входа
 
-#main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # клик по кнопке войти в аккаунт на главной странице
+class LoginLocators:
+    # Локаторы для входа по кнопке "Войти в аккаунт"
+    BUTTON_ENTER = (By.XPATH, "//button[text()='Войти в аккаунт']")   # ссылка для входа в личный кабинет на главной странице
+    EMAIL_INPUT = (By.XPATH, "(//input[@name='name'])")              # поле емайл
+    PASSWORD_INPUT = (By.NAME, "Пароль")                             # поле пароль
+    SUBMIT_LOGIN = (By.XPATH, "//button[text()='Войти']")            # кнопка войти
+    ORDER_BUTTON = (By.XPATH, "//button[contains(text(),'Оформить заказ')]")  # кнопка проверки входа
 
-#main_page.find_element(By.CLASS_NAME, "Auth_link__1fOlj").click() # клик по кнопке зарегистрироваться на странице входа в аккаунт
+    #Локаторы для входа через "Личный кабинет"
+    ACCOUNT_LINK = (By.XPATH, "//p[text()='Личный Кабинет']")        # кнопка личный кабинет
 
-#заполннение полей
-    #main_page.find_element(By.XPATH, "(//input[@name='name'])[1]").send_keys(valid_user["name"])    #поле имя 
-    #main_page.find_element(By.XPATH, "(//input[@name='name'])[2]").send_keys(valid_user["email"])   #поле емайл
-    #main_page.find_element(By.NAME, "Пароль").send_keys(valid_user["password"])                     #поле пароль  
+    #Локаторы для входа через форму регистрации
+    REGISTER_LINK = (By.CLASS_NAME, "Auth_link__1fOlj")              # ссылка на регистрацию
+    LOGIN_LINK_REGISTER = (By.XPATH, "//a[text()='Войти']")          # кнопка войти на форме регистрации
 
-#main_page.find_element(By.XPATH, "//button[text()='Зарегистрироваться']").click() # клик по кнопке зарегистрироваться на странице формы регистрации
-
-#authorized_main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click() #клик по кнопке личный кабинет на главной странице зарегистрированного пользователя
-
-#authorized_main_page.find_element(By.XPATH, "//p[text()='Конструктор']").click() #клик по кнопке конструктор на главной странице зарегистрированного пользователя
-
-
-#authorized_main_page.find_element(By.CLASS_NAME, "AppHeader_header__logo__2D0X2").click() #клик по логотипу Stellar Burgers
-
-#authorized_main_page.find_element(By.XPATH, "//button[text()='Выход']").click()  # клик по кнопке выход из личного кабинета
-
-#main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # клик по кнопке войти в форме входа в личный кабинет
-
-#authorized_main_page.find_element(By.XPATH, "//span[text()='Соусы']").click() # клик по кнопке переход в раздел соусы
-
-#authorized_main_page.find_element(By.XPATH, "//span[text()='Булки']").click() # клик по кнопке переход в раздел булки
-
-#authorized_main_page.find_element(By.XPATH, "//span[text()='Начинки']").click() # клик по кнопке переход в раздел начинки
-#1
+    #Локаторы для входа через форму восстановления пароля
+    RECOVER_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")  # ссылка восстановить пароль
+    LOGIN_LINK_RECOVER = (By.XPATH, "//a[text()='Войти']")            # кнопка войти на форме восстановления
