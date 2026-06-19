14ff449b882d07c40e6ef530c83b93565d1207ec
sisizov-dev <sisizov@gmail.com>
2026-06-19 21:43:22 +0100

    add_test_registration_invalid_password.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,7 +7,7 @@ class TestRegistration:
 
         main_page.find_element(*RegistrationLocators.BUTTON_ENTER).click() # нажимаем кнопку войти в аккаунт
 
-        main_page.find_element(*RegistrationLocators.BUTTON_CHEK).click() # нажимаем кнопку зарегистрироваться
+        main_page.find_element(*RegistrationLocators.REGISTER_LINK).click() # нажимаем ссылку на регистрацию
 
     #заполняем поля
         main_page.find_element(*RegistrationLocators.INP_NAME).send_keys(valid_user["name"])    #поле имя 
@@ -21,20 +21,20 @@ class TestRegistration:
 
 # добавляем негативный тест с невалидным паролем
 
-    #def test_registration_invalid_password(main_page, valid_user):
+    def test_registration_invalid_password(self, main_page, valid_user):
 
-     #   valid_user["password"] = "123" 
+        valid_user["password"] = "123" 
 
-      #  main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
+        main_page.find_element(*RegistrationLocators.BUTTON_ENTER).click() # нажимаем кнопку войти в аккаунт
 
-       # main_page.find_element(By.CLASS_NAME, "Auth_link__1fOlj").click() # нажимаем кнопку зарегистрироваться
+        main_page.find_element(*RegistrationLocators.REGISTER_LINK).click() # нажимаем ссылку на регистрацию
 
     #заполняем поля
-        #main_page.find_element(By.XPATH, "(//input[@name='name'])[1]").send_keys(valid_user["name"])    #поле имя 
-        #main_page.find_element(By.XPATH, "(//input[@name='name'])[2]").send_keys(valid_user["email"])   #поле емайл
-        #main_page.find_element(By.NAME, "Пароль").send_keys(valid_user["password"])                     #поле пароль  
+        main_page.find_element(*RegistrationLocators.INP_NAME).send_keys(valid_user["name"])    #поле имя 
+        main_page.find_element(*RegistrationLocators.INP_EMAIL).send_keys(valid_user["email"])   #поле емайл
+        main_page.find_element(*RegistrationLocators.INP_PAS).send_keys(valid_user["password"]) #поле пароль 
 
-        #main_page.find_element(By.XPATH, "//button[text()='Зарегистрироваться']").click() # нажимаем кнопку зарегистрироваться
+        main_page.find_element(*RegistrationLocators.BUTTON_CHEKIN).click() # нажимаем кнопку зарегистрироваться
 
-        #error_message = main_page.find_element(By.XPATH, "//p[contains(text(),'пароль')]")  # проверяем вывод сообщения об ошибке
-        #assert error_message.is_displayed()   
+        error_message = main_page.find_element(*RegistrationLocators.MESSAGE_ERROR)  # проверяем вывод сообщения об ошибке
+        assert error_message.is_displayed()   
