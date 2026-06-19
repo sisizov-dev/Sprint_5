d5a5a867c7f13cc7c5d6addf48d7ab210cec3f6a
sisizov-dev <sisizov@gmail.com>
2026-06-19 23:22:16 +0100

    upgrade_test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,55 +1,45 @@
-from selenium.webdriver.common.by import By
-import time
-def test_login_main_button(main_page, existing_user): #проверяем вход по кнопке «Войти в аккаунт» на главной странице
-    main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import LoginLocators
 
-    #заполняем поля
-    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
-    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
+class TestLogin:
+    def test_login_main_button(self, main_page, existing_user):  # проверяем вход по кнопке «Войти в аккаунт» на главной
+        main_page.find_element(*LoginLocators.BUTTON_ENTER).click()   # нажимаем кнопку войти в аккаунт
+        main_page.find_element(*LoginLocators.EMAIL_INPUT).send_keys(existing_user["email"])   # поле емайл
+        main_page.find_element(*LoginLocators.PASSWORD_INPUT).send_keys(existing_user["password"])  # поле пароль
+        main_page.find_element(*LoginLocators.SUBMIT_LOGIN).click()   # нажимаем кнопку войти
+        wait = WebDriverWait(main_page, 5)
+        order_button = wait.until(EC.visibility_of_element_located(LoginLocators.ORDER_BUTTON))
+        assert order_button.is_displayed()
 
-    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
+    def test_login_account_icon(self, main_page, existing_user):  # проверяем вход через кнопку «Личный кабинет»
+        main_page.find_element(*LoginLocators.ACCOUNT_LINK).click()   # нажимаем кнопку личный кабинет на главной
+        main_page.find_element(*LoginLocators.EMAIL_INPUT).send_keys(existing_user["email"])   # поле емайл
+        main_page.find_element(*LoginLocators.PASSWORD_INPUT).send_keys(existing_user["password"])  # поле пароль
+        main_page.find_element(*LoginLocators.SUBMIT_LOGIN).click()   # нажимаем кнопку войти
+        wait = WebDriverWait(main_page, 5)
+        order_button = wait.until(EC.visibility_of_element_located(LoginLocators.ORDER_BUTTON))
+        assert order_button.is_displayed()
 
-    time.sleep(4) 
-    assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
-    
-    
+    def test_login_from_registration_page(self, main_page, existing_user):  # проверяем вход через кнопку на форме регистрации
+        main_page.find_element(*LoginLocators.BUTTON_ENTER).click()   # нажимаем кнопку войти в аккаунт
+        main_page.find_element(*LoginLocators.REGISTER_LINK).click()   # нажимаем ссылку на регистрацию
+        main_page.find_element(*LoginLocators.LOGIN_LINK_REGISTER).click()   # нажимаем кнопку войти на форме регистрации
+        main_page.find_element(*LoginLocators.EMAIL_INPUT).send_keys(existing_user["email"])   # поле емайл
+        main_page.find_element(*LoginLocators.PASSWORD_INPUT).send_keys(existing_user["password"])  # поле пароль
+        main_page.find_element(*LoginLocators.SUBMIT_LOGIN).click()   # нажимаем кнопку войти
+        wait = WebDriverWait(main_page, 5)
+        order_button = wait.until(EC.visibility_of_element_located(LoginLocators.ORDER_BUTTON))
+        assert order_button.is_displayed()
 
-def test_login_account_icon(main_page, existing_user): #проверяем вход через кнопку «Личный кабинет»
-    main_page.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click() # нажимаем кнопку личный кабинет на главной странице
-
-    #заполняем поля
-    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
-    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
-
-    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
-
-    time.sleep(4) 
-    assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
-
-def test_login_from_registration_page(main_page, existing_user): #проверяем в ход в личный кабинет через кнопку войти на стрнице регистрации
-    main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
-    main_page.find_element(By.CLASS_NAME, "Auth_link__1fOlj").click() # нажимаем кнопку зарегистрироваться
-    main_page.find_element(By.XPATH, "//a[text()='Войти']").click() #нажимаем кнопку
-
-    #заполняем поля
-    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
-    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
-
-    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
-
-    time.sleep(4) 
-    assert len(main_page.find_elements(By.XPATH, "//button[contains(text(),'Оформить заказ')]")) > 0
-
-def test_login_from_password_recovery(main_page, existing_user): # проверка входа через кнопку в форме восстановления пароля
-    main_page.find_element(By.XPATH, "//button[text()='Войти в аккаунт']").click() # нажимаем кнопку войти в аккаунт
-    main_page.find_element(By.XPATH, "//a[text()='Восстановить пароль']").click() #нажимаем кнопку восстановить пароль
-    main_page.find_element(By.XPATH, "//a[text()='Войти']").click() #нажимаем кнопку Войти
-
-    #заполняем поля
-    main_page.find_element(By.XPATH, "(//input[@name='name'])").send_keys(existing_user["email"])   #поле емайл
-    main_page.find_element(By.NAME, "Пароль").send_keys(existing_user["password"])                     #поле пароль  
-
-    main_page.find_element(By.XPATH, "//button[text()='Войти']").click() # нажимаем кнопку войти
-
-    time.sleep(4) 
-    assert main_page.current_url == "https://stellarburgers.education-services.ru/"
+    def test_login_from_password_recovery(self, main_page, existing_user):  # проверка входа через форму восстановления
+        main_page.find_element(*LoginLocators.BUTTON_ENTER).click()   # нажимаем кнопку войти в аккаунт
+        main_page.find_element(*LoginLocators.RECOVER_PASSWORD_LINK).click()   # нажимаем ссылку восстановить пароль
+        main_page.find_element(*LoginLocators.LOGIN_LINK_RECOVER).click()   # нажимаем кнопку войти на форме восстановления
+        main_page.find_element(*LoginLocators.EMAIL_INPUT).send_keys(existing_user["email"])   # поле емайл
+        main_page.find_element(*LoginLocators.PASSWORD_INPUT).send_keys(existing_user["password"])  # поле пароль
+        main_page.find_element(*LoginLocators.SUBMIT_LOGIN).click()   # нажимаем кнопку войти
+        wait = WebDriverWait(main_page, 5)
+        order_button = wait.until(EC.visibility_of_element_located(LoginLocators.ORDER_BUTTON))
+        assert order_button.is_displayed()
