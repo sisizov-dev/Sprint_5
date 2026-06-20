62ca98b3fb8bcf96303eef9df59ab5f069c87b36
sisizov-dev <sisizov@gmail.com>
2026-06-20 22:02:22 +0100

    upgrade_test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,20 +1,28 @@
-from selenium.webdriver.common.by import By
-import time
-def test_sauces_section(authorized_main_page): # проверяем переход к разделу соусы
-    authorized_main_page.find_element(By.XPATH, "//span[text()='Соусы']").click() # переходим в раздел соусы
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import ConstructorLocators
 
-    time.sleep(2)
-    assert authorized_main_page.find_element(By.XPATH, "//*[@alt='Соус фирменный Space Sauce']").is_displayed()
+class TestConstructor:
 
-def test_buns_section(authorized_main_page):  # проверяем переход к разделу булки
-    authorized_main_page.find_element(By.XPATH, "//span[text()='Соусы']").click() # переходим в раздел соусы
-    authorized_main_page.find_element(By.XPATH, "//span[text()='Булки']").click() # переходим в раздел булки
+    def test_sauces_section(self, authorized_main_page): # проверяем переход к разделу соусы
+        authorized_main_page.find_element(*ConstructorLocators.SAUCES_TAB).click() #переходиим в раздел соусы
 
-    time.sleep(2)
-    assert authorized_main_page.find_element(By.XPATH, "//*[@alt='Флюоресцентная булка R2-D3']").is_displayed()
+        wait = WebDriverWait(authorized_main_page, 5)
+        active_tab = wait.until(EC.visibility_of_element_located(ConstructorLocators.ACTIVE_TAB))
+        assert "Соусы" in active_tab.text
 
-def test_fillings_section(authorized_main_page):  # проверяем переход к разделу начинки
-    authorized_main_page.find_element(By.XPATH, "//span[text()='Начинки']").click() # переходим в раздел начинки
+    def test_buns_section(self, authorized_main_page):  # проверяем переход к разделу булки
+        authorized_main_page.find_element(*ConstructorLocators.SAUCES_TAB).click() #переходиим в раздел соусы
+        authorized_main_page.find_element(*ConstructorLocators.BUNS_TAB).click() # переходим в раздел булки
 
-    time.sleep(2)
-    assert authorized_main_page.find_element(By.XPATH, "//*[@alt='Говяжий метеорит (отбивная)']").is_displayed()
+        wait = WebDriverWait(authorized_main_page, 5)
+        active_tab = wait.until(EC.visibility_of_element_located(ConstructorLocators.ACTIVE_TAB))
+        assert "Булки" in active_tab.text
+
+    def test_fillings_section(self, authorized_main_page):  # проверяем переход к разделу начинки
+        authorized_main_page.find_element(*ConstructorLocators.FILLINGS_TAB).click() # переходим в раздел начинки
+
+        wait = WebDriverWait(authorized_main_page, 5)
+        active_tab = wait.until(EC.visibility_of_element_located(ConstructorLocators.ACTIVE_TAB))
+        assert "Начинки" in active_tab.text
