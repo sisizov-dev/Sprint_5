4148454290bc7e6a1885dd13d18d60d259385f7c
sisizov-dev <sisizov@gmail.com>
2026-06-20 22:00:20 +0100

    add_ConstructorLocators

--- tests/locators.py
+++ tests/locators.py
@@ -50,3 +50,11 @@ class LogoutLocators:
     #Локаторы для выхода из аккаунта
     PERSONAL_ACCOUNT_LINK = (By.XPATH, "//p[text()='Личный Кабинет']")   # кнопка личный кабинет
     LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выход']")               # кнопка выход из личного кабинета
+
+class ConstructorLocators:
+    #Локаторы для перехода к разделам булки, соусы, начинки
+    BUNS_TAB = (By.XPATH, "//span[text()='Булки']")                # вкладка «Булки»
+    SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']")              # вкладка «Соусы»
+    FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']")         # вкладка «Соусы»
+
+    ACTIVE_TAB = (By.XPATH, "//div[contains(@class, 'tab_tab_type_current__2BEPc ')]") #класс активного таба
